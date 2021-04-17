dd3582d06526beafda991ded012901bb8c0c3bd5
VasiaChernobay <vasia1chernobay1@gmail.com>
2021-04-17 14:23:36 +0300

    fist commit

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,52 +1,40 @@
 import {Component,Fragment} from "react";
 import { Container} from "react-bootstrap";
-import './scheduler.css';
-import AddIcon from "@material-ui/icons/Add"
+import './scheduler.css'
+import AddIcon from '@material-ui/icons/Add';
 import {Button, Fab, makeStyles, Paper, TextField} from "@material-ui/core";
 import * as ReactDOM from "react-dom";
 
 
-    class Scheduler extends Component {
-     constructor(props) {
-         super(props);
-         this.onHandleClick=this.onHandleClick.bind(this)
-     }
-    onHandleClick(){
-         return ReactDOM.render(<MenuAddedAppointment/>,document.getElementById("1"));
+class Scheduler extends Component {
+    constructor(props) {
+        super(props);
+
     }
-     render() {
+
+    render() {
 
 
-         return <Container>
-             <Table/>
-             <Fab color="primary" aria-label="add" onClick={this.onHandleClick}>
-                 <AddIcon/>
-             </Fab>
-         </Container>
-     }
- }
+        return <Container>
+            <Table/>
+        </Container>
+    }
+}
 const Table=()=>{
     return <div className={"table-responsive"}>
         <table className={"table table-bordered text-center"}>
-              <TheadTable/>
-             <TbodyTable/>
+            <TheadTable/>
+            <TbodyTable/>
         </table>
     </div>
 }
 const TheadTable=()=>{
     return <Fragment>
         <thead>
-        {/*<tr className={"bg-light-gray"}>*/}
-        {/*    <th className={"text-uppercase"}>Time</th>*/}
-        {/*    <th className={"text-uppercase"}>Monday</th>*/}
-        {/*    <th className={"text-uppercase"}>Tuesday</th>*/}
-        {/*    <th className={"text-uppercase"}>Wednesday</th>*/}
-        {/*    <th className={"text-uppercase"}>Thursday</th>*/}
-        {/*    <th className={"text-uppercase"}>Friday</th>*/}
-        {/*    <th className={"text-uppercase"}>Saturday</th>*/}
-        {/*</tr>*/}
+
         <tr className={"bg-light-gray"}>
-            <th className={"text-uppercase"}>Time</th>
+            <th className={"text-uppercase"}>Locations</th>
+
             <th className={"text-uppercase"}>10:00am</th>
             <th className={"text-uppercase"}>11:00am</th>
             <th className={"text-uppercase"}>12:00am</th>
@@ -67,7 +55,7 @@ const TbodyTable=()=>{
         <tbody>
         <tr>
             <td className={"align-middle"}>
-               Mon
+                Gym
             </td>
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
@@ -83,9 +71,9 @@ const TbodyTable=()=>{
 
         </tr>
         <tr>
-            <td className={"align-middle"}>Tue</td>
+            <td className={"align-middle"}>Jobs</td>
 
-           
+
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
@@ -100,7 +88,7 @@ const TbodyTable=()=>{
 
         </tr>
         <tr>
-            <td className={"align-middle"}>Wed</td>
+            <td className={"align-middle"}>Shop</td>
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
@@ -113,112 +101,26 @@ const TbodyTable=()=>{
             <td className={"bg-light-gray"}></td>
             <td className={"bg-light-gray"}></td>
         </tr>
-        <tr>
-            <td className={"align-middle"}>Thu</td>
-            <td className={"bg-light-gray"}>
-                <Appointment name={"vasia"} target={"Go to gym" } time={"10:00-11:00"}/>
-            </td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
 
-        </tr>
-        <tr><td className={"align-middle"}>Fri</td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-
-        </tr>
-        <tr><td className={"align-middle"}>Sat</td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-        </tr>
-        <tr><td className={"align-middle"}>Sud</td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-            <td className={"bg-light-gray"}></td>
-        </tr>
 
         </tbody>
     </Fragment>
 }
 class Appointment extends Component{
-     constructor(props) {
-         super(props);
-
-     }
-     render(){
-         return <Fragment>
-             <span className={"bg-yellow padding-5px-tb padding-15px-lr border-radius-5 margin-10px-bottom text-white font-size16  xs-font-size13"}>{this.props.target}</span>
-             <div className={"margin-10px-top font-size14"}>{this.props.time}</div>
-             <div className={"font-size13 text-light-gray"}>{this.props.name}</div>
-
-         </Fragment>
-     }
-}
-
-export class MenuAddedAppointment extends  Component{
-     
     constructor(props) {
         super(props);
-    }
-    
-    render() {
 
-        return <Paper elevation={3} style={{height:"500px",width:"400px"}}>
-            <form noValidate>
-                <TextField
-                    id="date"
-                    label="Start Target"
-                    type="date"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-            </form>
-            <form>
-                <TextField id="date" label={"End Target"} type={"date"} InputLabelProps={{
-                    shrink: true,
-                }}/>
-                <TextField id="outlined-basic" label="Target" variant="outlined" />
-                <Button variant="contained" color="primary">
-                    Create
-                </Button>
-            </form>
-        </Paper>
+    }
+    render(){
+        return <Fragment>
+            <span className={"bg-yellow padding-5px-tb padding-15px-lr border-radius-5 margin-10px-bottom text-white font-size16  xs-font-size13"}>{this.props.target}</span>
+            <div className={"margin-10px-top font-size14"}>{this.props.time}</div>
+            <div className={"font-size13 text-light-gray"}>{this.props.name}</div>
+
+        </Fragment>
     }
 }
 
-export default Scheduler
+
+
+export  default Scheduler
